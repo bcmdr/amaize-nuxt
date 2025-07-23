47828158e8c71c92221aba8d6228b562814d31f7
Brett Commandeur <bcommandeur@gmail.com>
2025-07-23 17:26:57 -0600

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,12 +1,12 @@
 <script setup>
 useHead({
   htmlAttrs: { lang: "en-US" },
-  title: "Nuxt Starter",
+  title: "Amaize | Get Started",
   meta: [
     {
       name: "description",
       content:
-        "Nuxt Starter is a minimal template created for quick landing pages, built with Nuxt and Tailwind CSS.",
+        "Get Unstuck. Start Small. Amaize Yourself.",
     },
   ],
   bodyAttrs: {
